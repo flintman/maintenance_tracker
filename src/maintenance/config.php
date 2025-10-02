c030d6ea6ef6de28f0359d79ce981588fc827921
William Bellavance Jr <flintman@flintmancomputers.com>
2025-10-02 15:56:47 -0400

    Password:  add some to the docker for testing and pull to apache
    Makes it simple for people to just change the password for the
    database in the docker and will update in config.php

--- src/maintenance/config.php
+++ src/maintenance/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'db';
-$db   = 'maintenance';
-$user = 'maintenance';
-$pass = 'ka*jdalK87jdaJ';
+$db   = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
